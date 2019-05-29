20fd5222af5511b85557aefa410bbb50c750920c
Edgars Rumkovskis <edgarsrumkovskis@inbox.lv>
2019-05-30 00:35:15 +0300

    - add configuration to run cron so that scheduler would run

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,6 @@ FROM php:fpm-alpine
 # Install extensions
 RUN docker-php-ext-install pdo_mysql
 
-# Expose port 9000 and start php-fpm server
+COPY root-crontab /var/spool/cron/crontabs/root
+
 EXPOSE 9000
-CMD ["php-fpm"]
--- app.sh
+++ app.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-#docker-php-ext-install pdo_mysql
-#Need to add ping check if DB has fully started
 
+#Need to add ping check if DB has fully started
 #Adapt for mysql
 #PSQL="psql -h localhost -p 5432 -U postgres -v ON_ERROR_STOP=1"
 #until $PSQL -c "select version()" &> /dev/null
@@ -14,4 +13,7 @@ echo 'Waiting 15 seconds for mySql server to fully start'
 sleep  15
 echo 'starting migration'
 php /var/www/laravel/artisan migrate
+echo 'Start up crond'
+crond start
+echo 'Start php-fpm server'
 php-fpm
